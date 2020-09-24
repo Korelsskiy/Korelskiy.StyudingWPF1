4c9354d9ff98afd1950e36831a1f534647fc9e9f
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-09-24 11:27:13 +0300

    Тестирование работы модели-представления

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Korelskiy.StyudingWPF1.ViewModels.Base;
+using System.Runtime.CompilerServices;
+
+namespace Korelskiy.StyudingWPF1.ViewModels
+{
+    internal class MainWindowViewModel: ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "Истребители на вооружении стран мира";
+
+        ///<summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
