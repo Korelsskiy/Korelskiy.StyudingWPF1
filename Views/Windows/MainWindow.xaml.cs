3ff4c0c8bb147419f5273359041331af854bc64d
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-09-24 09:49:35 +0300

    Базовый класс модели-представления

---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Korelskiy.StyudingWPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
