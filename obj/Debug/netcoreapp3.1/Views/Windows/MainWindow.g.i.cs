0707b3e5458d7793ffc40ffc37080e49ab025093
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-09-28 23:27:19 +0300

    Доработка

--- obj/Debug/netcoreapp3.1/Views/Windows/MainWindow.g.i.cs
+++ obj/Debug/netcoreapp3.1/Views/Windows/MainWindow.g.i.cs
@@ -74,7 +74,7 @@ namespace Korelskiy.StyudingWPF1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Korelskiy.StyudingWPF1;V1.0.0.0;component/views/windows/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Korelskiy.StyudingWPF1;component/views/windows/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Windows\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
